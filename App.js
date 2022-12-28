ad53255ccc6494600f1730165f0d22dc994f477f
E-Zbox <zboxstudios97@gmail.com>
2022-12-28 13:45:24 +0100

    Entry file included

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// expo
+import Constants from "expo-constants";
+import { StatusBar } from "expo-status-bar";
+// navigator
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// react
+import React from "react";
+import { StyleSheet } from "react-native";
+// routes
+import MainNavigator from "./routes/MainNavigator";
+// utils
+import { colors } from "./utils/data";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+    return (
+        <>
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="MainNavigator"
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: colors.white01,
+                        },
+                        headerShown: false,
+                    }}
+                >
+                    <Stack.Screen
+                        name="MainNavigator"
+                        component={MainNavigator}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+            <StatusBar style="light" backgroundColor={colors.blue02} />
+        </>
+    );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: Constants.statusBarHeight,
+    },
 });
